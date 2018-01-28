b8286ed128c62929e10de6dcb5e848362ff1fd57
Therdpong Khangrang <numlifez@icloud.com>
2018-01-28 22:18:38 +0700

    Update index.php

--- web-script/index.php
+++ web-script/index.php
@@ -3,10 +3,13 @@
     require_once 'init.php';
     include_once 'include/header.php';
     include_once 'include/navbar.php';
+
     if (empty($_GET['view'])) {
     	include_once 'include/list.php';
     }elseif($_GET['view'] == 'upload'){
     	include_once 'include/upload.php';
+    }elseif($_GET['view'] == 'chart'){
+        include_once 'include/chart.php';
     }else{
     	include_once 'include/info.php';
     }
